d8cdc559c335357dd7ee77d62144ef4dca018f72
ismail ait hsaine <92794718+aithsaine@users.noreply.github.com>
2024-08-17 12:11:12 +0100

    issues4

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -3,7 +3,7 @@ import prisma from '../../../../prisma/client';
 import { connect } from '../../../../helpers/connectToDatabase';
 import bcrypt from 'bcryptjs';
 
-export async function POST(request: Request) {
+export async function POST(request: NextRequest) {
     try {
         const { firstname, lastname, email, password, confirmPsd } = await request.json();
 
@@ -16,26 +16,32 @@ export async function POST(request: Request) {
         }
 
         await connect();
-        const salt = await bcrypt.genSalt(10)
-
+        const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
-        if (await prisma.user.findFirst({ where: { email } })) {
-            return NextResponse.json({ success: false, message: 'Email Already in use!!' }, { status: 422 });
 
+        const existingUser = await prisma.user.findFirst({ where: { email } });
+        if (existingUser) {
+            return NextResponse.json({ success: false, message: 'Email already in use!!' }, { status: 422 });
         }
+
         const newUser = await prisma.user.create({
-            data: { firstname: String(firstname), lastname: String(lastname), email: String(email), password: hashedPassword },
+            data: {
+                firstname: String(firstname),
+                lastname: String(lastname),
+                email: String(email),
+                password: hashedPassword,
+            },
         });
 
         if (newUser) {
             return NextResponse.json({ success: true, message: 'Account created successfully' });
+        } else {
+            return NextResponse.json({ success: false, message: 'Sorry! Something went wrong!!' }, { status: 400 });
         }
-        return NextResponse.json({ success: false, message: 'Sorry! Something went wrong!!' }, { status: 400 });
 
     } catch (error) {
-        return NextResponse.json({ success: false, message: 'Sorry! Something went wrong!!', error }, { status: 400 });
+        return NextResponse.json({ success: false, message: 'Sorry! Something went wrong!!' }, { status: 400 });
     } finally {
         await prisma.$disconnect();
     }
 }
-
